30645d5948040d99443ed533c0abb19f9c50d376
Jefferson <jeffersoncastiblanco@gmail.com>
2014-04-10 00:09:45 -0500

    Modelo MVC - Plantilla
    Se agrega pagina maestra, se crea conexion con la BD y su respectivo
    controlador. No se creo la BD por necesidad de discusión

---
+++ FE_ServTesoft/Controllers/InicioController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FE_ServTesoft.Controllers
+{
+    public class InicioController : Controller
+    {
+        //
+        // GET: /Inicio/
+
+        public ActionResult Plantilla()
+        {
+            return View();
+        }
+
+    }
+}
